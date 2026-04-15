dd3b8e6188afbc7ab0bb5d547cefc269e572783b
saloni verma <saloniv375@gmail.com>
2026-04-15 23:33:07 +0530

    mongoose

--- db.js
+++ db.js
@@ -1,10 +1,5 @@
-// db.js
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-  console.log('MongoDB connected successfully ✅');
-})
-.catch((err) => {
-  console.error('MongoDB connection error ❌', err);
-});
+.then(() => console.log("MongoDB connected ✅"))
+.catch(err => console.log("MongoDB connection error ❌", err));
