9cac2ebf83bf89a0b0f7c4214a2f2c3b05e89178
saloni verma <saloniv375@gmail.com>
2026-04-15 22:50:50 +0530

    updated

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 // db.js
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/laundryDB')
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
   console.log('MongoDB connected successfully ✅');
 })
